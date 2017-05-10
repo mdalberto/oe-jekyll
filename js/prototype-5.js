99a6e82ccf4d0ff146036e9fc91f40678ef9aea9
mdal138 <mdal138@gmail.com>
2017-05-10 15:25:30 -0400

    tweaks to protoype 5

--- js/prototype-5.js
+++ js/prototype-5.js
@@ -1,11 +1,27 @@
 // move the transcript bar over
 
 $(document).ready(function() {
+
   $('#btn-transcript').click(function() {
+      var sidebar = $("#sidebar");
       var left2 = $('#sidebar').offset().left;
             var left = $('#sidebar').width();
             var totalslide = left + left2;
-       $("#sidebar").animate({"left" : "-" + totalslide}, "fast").removeClass("col-12 col-md-5 col-lg-4").css('position', 'absolute');
- 
+            
+       sidebar.animate({"left" : "-" + (left - 10)}, "fast").removeClass("col-12 col-md-5 col-lg-4").css('position', 'absolute').css('width', left + 'px');
+        console.log(left2);
+                $(this).hide();
+        $('#btn-transcript2').show();
+    });
+    $('#btn-transcript2').click(function() {
+      var sidebar = $("#sidebar");
+      var left2 = $('#sidebar').offset().left;
+            var left = $('#sidebar').width();
+            var totalslide = left + left2;
+            
+       sidebar.addClass("col-12 col-md-5 col-lg-4").removeAttr("style");
+        console.log(left2);
+        $(this).hide();
+        $('#btn-transcript').show();
     });
 });
